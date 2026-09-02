b4aa4930db52a345110eb1ece855b33aaa21ecf0
Issa Assi <issa.assi06@gmail.com>
2026-09-02 12:42:27 +0300

    Add Customer entity

--- src/CustomerOrder.Domain/Entities/Customer.cs
+++ src/CustomerOrder.Domain/Entities/Customer.cs
@@ -3,7 +3,6 @@ namespace CustomerOrder.Domain.Entities;
 public class Customer
 {
     public int Id { get; set; }
-
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
@@ -14,4 +13,4 @@ public class Customer
     public string? UpdatedBy { get; set; }
     public bool IsActive { get; set; } = true;
     public bool IsDeleted { get; set; } = false;
-}
+} 
